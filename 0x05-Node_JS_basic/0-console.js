5e8fc240c90ef991a2842a23bea0eb9b83e1a057
NdiranguAndrew43 <andymurunga@gmail.com>
2024-11-26 20:29:27 +0000

    Node JS all tasks completed

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message to STDOUT
+ * @param {String} msg: The message to display
+ * @author Andrew Ndirangu <https://github.com/NdiranguAndrew43>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
